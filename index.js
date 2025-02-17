4a28b5857aa7f63f97ccc18faa0c79e00f13b4fb
maham535 <bibimaham@gmail.com>
2025-02-17 15:19:30 +0500

    'firs'

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const users = [
+    { id: 1, name: 'maham', age: 25 },
+    { id: 2, name: 'sadaf', age: 30 },
+    { id: 3, name: 'arzoo', age: 35 },
+  ];
+  const userNames = users.map(user => user.name);
+  console.log(userNames); 
