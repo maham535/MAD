c981a36b346575f37aa98b3d77f1249bf10feb5d
maham535 <bibimaham@gmail.com>
2025-02-19 07:23:27 +0500

    'function'

---
+++ typeOfunction.js
@@ -0,0 +1,10 @@
+function greet() {
+    console.log("Hello!");
+}
+
+let sum = function(a, b) {
+    return a + b;
+};
+
+console.log(typeof greet); // Output: "function"
+console.log(typeof sum);   // Output: "function"
